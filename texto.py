bcc838cdf4441d3eb64ed3c99f167a46e35a88b6
ElecBen <148629785+ElecBen@users.noreply.github.com>
2025-03-04 19:47:00 +0000

    Add envuelve()

---
+++ texto.py
@@ -0,0 +1,21 @@
+import unicodedata
+
+
+MENUDAS = {"a", "con", "de", "del", "el", "en", "la", "las",
+           "los", "o", "por", "y"}
+
+
+def envuelve(texto, ancho=72):
+    """Parte el texto en lineas de `ancho` caracteres como maximo."""
+    lineas, actual = [], ""
+    for palabra in texto.split():
+        if not actual:
+            actual = palabra
+        elif len(actual) + 1 + len(palabra) <= ancho:
+            actual += " " + palabra
+        else:
+            lineas.append(actual)
+            actual = palabra
+    if actual:
+        lineas.append(actual)
+    return lineas
